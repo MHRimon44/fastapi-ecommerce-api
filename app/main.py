0558b73b93ac1225758554a09a035a2d1fef8c49
MHRimon44 <mehedihasanrimon@gmail.com>
2026-06-25 14:56:23 +0600

    fix: CORS emplemented

--- app/main.py
+++ app/main.py
@@ -1,30 +1,42 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from app.api.v1.customer_routes import router as customer_router
-from app.api.v1.order_routes import router as order_router
-from app.api.v1.product_routes import router as product_router
-from app.api.v1.payment_routes import router as payment_router
-from app.api.v1.voucher_routes import router as voucher_router
-from app.api.v1.auth_routes import router as auth_router
-from app.api.v1.background_routes import router as background_router
-
+from app.core.config import settings
 from app.core.exception_handlers import (
     http_exception_handler,
     validation_exception_handler,
 )
 
+from app.api.v1.product_routes import router as product_router
+from app.api.v1.customer_routes import router as customer_router
+from app.api.v1.order_routes import router as order_router
+from app.api.v1.payment_routes import router as payment_router
+from app.api.v1.voucher_routes import router as voucher_router
+from app.api.v1.auth_routes import router as auth_router
+from app.api.v1.background_routes import router as background_router
+
+
 app = FastAPI(
-    title="FastAPI E-commerce API",
-    version="1.0.0",
+    title=settings.APP_NAME,
+    debug=settings.DEBUG,
 )
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.add_exception_handler(
     StarletteHTTPException,
     http_exception_handler,
 )
-
 app.add_exception_handler(
     RequestValidationError,
     validation_exception_handler,
